dacf1b7ea9b487df2c61533fa3476dae46d83099
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2016-03-26 13:43:58 -0500

    one endpoint, halfway done

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
 (defproject capmetro-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "REST API for undocumented CapMetro API"
+  :url "http://bradshaw-rpi.ddns.net/capmetro"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.5"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+		 [ring/ring-json "0.4.0"]
+		 [clj-http "2.1.0"]
+		 [cheshire "5.0.2"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler capmetro-clj.handler/app}
   :profiles
--- src/capmetro_clj/handler.clj
+++ src/capmetro_clj/handler.clj
@@ -1,10 +1,15 @@
 (ns capmetro-clj.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+	    [clojure.data.json :as json]
+	    [clj-http.client :as client]))
 
+(defn get-next-bus [stopid]
+  (:body (client/get "http://www.capmetro.org/planner/s_nextbus2.asp" {:accept :json ,:query-params {"stopid" stopid, "opt" 1}})))
+  
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (get-next-bus 1042))
   (route/not-found "Not Found"))
 
 (def app
