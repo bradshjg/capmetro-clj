2969bf24cbee81adbe1df7fb98c7ef3cdd5c55ff
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2016-03-27 13:59:47 -0500

    pretty sloppy but working, now have two endpoints that always return 200 responses because why not :-)

--- src/capmetro_clj/handler.clj
+++ src/capmetro_clj/handler.clj
@@ -7,9 +7,6 @@
             [clojure.data.json :as json]
             [clj-http.client :as client]))
 
-(defn get-next-bus [stopid]
-  (client/get "http://www.capmetro.org/planner/s_nextbus2.asp" {:accept :json ,:query-params {"stopid" stopid, "opt" 1}}))
-
 (defn extract-body [response]
   (json/read-str (:body response)))
 
@@ -17,11 +14,41 @@
   {:body body,
    :status 200})
 
+(defn capmetro-api [endpoint params]
+  (let [endpoint-map {:stop "http://www.capmetro.org/planner/s_locate.asp",
+                      :next   "http://www.capmetro.org/planner/s_nextbus2.asp"}]
+  (-> (client/get ((keyword endpoint) endpoint-map) {:query-params params})
+      (extract-body))))
+
+(defn get-stop [id]
+  (-> (capmetro-api "stop" {"loc" id, "opt" 1})
+      (make-response)))
+
+(defn get-next-bus [id]
+  (-> (capmetro-api "next" {"stopid" id, "opt" 3})
+      (make-response)))
+
 (defroutes app-routes
-  (GET "/" [] (-> (get-next-bus 1042) (extract-body) (make-response)))
-  (GET "/test" [] (:one {:one "one", :two "two"}))
+  (GET "/stop" [] "Still need to get all stops")
+  (GET "/stop/:id" [id] (get-stop id))
+  (GET "/stop/:id/next" [id] (get-next-bus id))
   (route/not-found "Not Found"))
+
+(defn ignore-trailing-slash
+  "Modifies the request uri before calling the handler.
+  Removes a single trailing slash from the end of the uri if present.
+
+  Useful for handling optional trailing slashes until Compojure's route matching syntax supports regex.
+  Adapted from http://stackoverflow.com/questions/8380468/compojure-regex-for-matching-a-trailing-slash"
+  [handler]
+  (fn [request]
+    (let [uri (:uri request)]
+      (handler (assoc request :uri (if (and (not (= "/" uri))
+                                            (.endsWith uri "/"))
+                                     (subs uri 0 (dec (count uri)))
+                                     uri))))))
 
 (def app
   (-> (handler/api app-routes)
+      (ignore-trailing-slash)
       (wrap-json-response)))
--- test/capmetro_clj/handler_test.clj
+++ test/capmetro_clj/handler_test.clj
@@ -4,8 +4,23 @@
             [capmetro-clj.handler :refer :all]))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
+  (testing "get stop"
+    (let [response (app (mock/request :get "/stop/1042"))]
+      (is (= (:status response) 200))
+      (is (= (get-in response [:headers "Content-Type"]) "application/json; charset=utf-8"))))
+
+  (testing "get stop with slash"
+    (let [response (app (mock/request :get "/stop/1042/"))]
+      (is (= (:status response) 200))
+      (is (= (get-in response [:headers "Content-Type"]) "application/json; charset=utf-8"))))
+
+  (testing "get next"
+    (let [response (app (mock/request :get "/stop/1042/next"))]
+      (is (= (:status response) 200))
+      (is (= (get-in response [:headers "Content-Type"]) "application/json; charset=utf-8"))))
+
+  (testing "get next with slash"
+    (let [response (app (mock/request :get "/stop/1042/next/"))]
       (is (= (:status response) 200))
       (is (= (get-in response [:headers "Content-Type"]) "application/json; charset=utf-8"))))
 
